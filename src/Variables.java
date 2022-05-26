e5373ed527bd3b5dd9564c69c78e1203fe2bd36e
mkhales30 <106184751+mkhales30@users.noreply.github.com>
2022-05-26 14:52:45 -0400

    Update Variables.java

--- src/Variables.java
+++ src/Variables.java
@@ -1,7 +1,7 @@
 /**
  * class name: Variables.java
- * author: ________
- * date:
+ * author: Khales M. Rahman
+ * date: 05/26/22
  * version:
  * description: This program will display summary of primitive data types.
  * source: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
@@ -15,6 +15,23 @@ public class Variables
     public static void main(String[] args)
     {
         System.out.println("Summary of The Primitive Data Types");
+        System.out.println(" A byte datatype is an 8-bit integer") 
+        System.out.println(" that can be useful for saving memory.");
+        System.out.println(" A short datatype is a 16-bit integer that can") 
+        System.out.println(" also be useful for saving memory in a large aray");
+        System.out.println(" A int datatype is a 32-bit integer and can be used for an unsigned integer.");
+        System.out.println(" A long datatype is a 64-bit integer and can be used when"); 
+        System.out.println(" you need a range of values wider than those provided by int.");
+        System.out.println(" A float 32-bit datatype IEEE 754 floating point."); 
+        System.out.println(" This data type should never be used for precise values");
+        System.out.println(" A double data type is double presicion 754 floating); 
+        System.out.println(" pointand is usually the default choise for decimals");
+        System.out.println(" A boolean only has two possible outcomes, true or false, ");
+        System.out.println("and this can be used to for simple flags that track true/false conditions."); 
+        System.out.println(" The char data type 16-bit Unicode character");
+        System.out.println(" and has a minimum value of '\u0000' (or 0) and a maximum value of '\uffff.");
+        
+        
     }
 
 }
