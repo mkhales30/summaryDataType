ba02b588a945984b5994d9ace47427b6fd8c712a
mkhales30 <106184751+mkhales30@users.noreply.github.com>
2022-05-27 14:57:54 -0400

    Create IntegerTest.java

---
+++ src/IntegerTest.java
@@ -0,0 +1,16 @@
+/** classs name: IntergerTest.Java
+*   author: Khales M. Rahma
+*   date: 05/26/22
+*   ddescription: 
+*   
+*
+*/
+
+public class IntegerTest
+{
+  public static void main(String[] args)
+  {
+    int number = 2147483648;
+    System.out.print(number +1);
+   
+}
